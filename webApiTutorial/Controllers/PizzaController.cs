886aeaa4087503ac9b20971a03366478eb25b6fb
void david <void9david@gmail.com>
2025-02-20 11:40:24 -0600

    Add project files.

---
+++ webApiTutorial/Controllers/PizzaController.cs
@@ -0,0 +1,26 @@
+﻿using webApiTutorial.Models;
+using webApiTutorial.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webApiTutorial.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+
+        }
+
+        // GET all action
+
+        // GET by Id action
+
+        // POST action
+
+        // PUT action
+
+        // DELETE action
+    }
+}
