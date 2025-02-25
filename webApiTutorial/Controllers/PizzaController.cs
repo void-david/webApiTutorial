204d363a3cfb86485138bf87965e8cfb4e3ba8ec
void david <void9david@gmail.com>
2025-02-24 18:19:05 -0600

    finished crud api

--- webApiTutorial/Controllers/PizzaController.cs
+++ webApiTutorial/Controllers/PizzaController.cs
@@ -10,17 +10,65 @@ namespace webApiTutorial.Controllers
     {
         public PizzaController()
         {
+           
 
         }
 
-        // GET all action
 
+        // GET all action
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
         // GET by Id action
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
 
         // POST action
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+        }
+
 
         // PUT action
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if(id != pizza.Id)
+            {
+                return BadRequest();
+            }
+
+            if(PizzaService.Get(id) == null)
+            {
+                return NotFound();
+            }
+            PizzaService.Update(pizza);
+            return NoContent();
+
+        }
 
         // DELETE action
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (PizzaService.Get(id) == null)
+            {
+                return NotFound();
+            }
+            PizzaService.Delete(id);
+            return NoContent();
+
+        }
+
     }
 }
